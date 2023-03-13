ddd35c507a0ea832b5350c14d29633eee170d71b
superkookai <weerawut@live.com>
2023-03-13 14:45:16 +0700

    try ternary operator

---
+++ trycode.js
@@ -0,0 +1,4 @@
+const password = "pas$word";
+
+password.length >= 12 ? console.log("password so strong WELCOME!") : 
+    password.length >= 8 ? console.log("WELCOME!!") : console.log("SORRY password is too short!");
